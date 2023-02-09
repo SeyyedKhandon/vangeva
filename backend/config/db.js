66eb25470cf3a4add6d10b25828635113958f532
SeyyedKhandon <seyyedkhandon@gmail.com>
2023-02-09 15:42:54 +0330

    feat: :sparkles: Add mongoose DB connection
    - .env
    NODE_ENV = development
    PORT = 5000
    MONGO_URI = mongodb+srv://user:password@yourcluser.mongodb.net/?retryWrites=true&w=majority

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+export default async function connectDB() {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,11 @@ import "colors";
 import express from "express";
 import userRoute from "./routes/users.js";
 import errorHandler from "./middleware/error.js";
-
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
 const app = express();
 
+connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", userRoute);
